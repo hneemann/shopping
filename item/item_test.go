9467a1ea02fe57c21df48ecafbb146ecd36947d1
hneemann <helmut.neemann@arcor.de>
2024-07-26 13:32:14 +0200

    added some test cases

--- item/item_test.go
+++ item/item_test.go
@@ -1,6 +1,10 @@
 package item
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
 
 func Test_shorten(t *testing.T) {
 	type args struct {
@@ -23,6 +27,45 @@ func Test_shorten(t *testing.T) {
 			if got := shorten(tt.args.s); got != tt.want {
 				t.Errorf("shorten() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestItems_removeOldHistory(t *testing.T) {
+	n := time.Now()
+	tests := []struct {
+		name string
+		item Item
+		size int
+	}{
+		{"empty", Item{
+			ShopHistory: []HistoryEntry{},
+		}, 0},
+		{"no remove", Item{
+			ShopHistory: []HistoryEntry{{n, 1}},
+		}, 1},
+		{"all", Item{
+			ShopHistory: []HistoryEntry{{n.Add(-24 * time.Hour * (historyDays + 1)), 1}},
+		}, 0},
+		{"one", Item{
+			ShopHistory: []HistoryEntry{
+				{n.Add(-24 * time.Hour * (historyDays + 1)), 1},
+				{n, 1},
+			},
+		}, 1},
+		{"two", Item{
+			ShopHistory: []HistoryEntry{
+				{n.Add(-24 * time.Hour * (historyDays + 2)), 1},
+				{n.Add(-24 * time.Hour * (historyDays + 1)), 1},
+				{n, 1},
+			},
+		}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var items Items = []*Item{&tt.item}
+			items.removeOldHistory()
+			assert.EqualValues(t, tt.size, len(items[0].ShopHistory))
 		})
 	}
 }
